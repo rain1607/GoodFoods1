11ef0b06f58cd196e6f9a822d60dcbc2ae1fa5be
rain1607 <namandfon7@gmail.com>
2019-11-20 02:13:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/goodfoods/MenuGoodfood.java
@@ -0,0 +1,14 @@
+package com.example.goodfoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuGoodfood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_goodfood);
+    }
+}
